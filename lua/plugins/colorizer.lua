4a653ac0fa0a41280285794beb34276288531f88
Kirill Solovey <kirasolovey09@gmail.com>
2025-07-15 16:22:31 +0500

    add keys for colorizer and gitsign

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,4 +1,20 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	event = "VeryLazy",
+	cmd = { "ColorizerToggle", "ColorizerAttachToBuffer", "ColorizerDetachFromBuffer", "ColorizerReloadAllBuffers" },
+	config = function()
+		require("colorizer").setup({
+			"*",
+			css = { css = true },
+		}, {
+			RRGGBBAA = true,
+		})
+		vim.cmd("ColorizerToggle")
+	end,
+	keys = {
+		{ "<leader>cc", "<cmd> ColorizerToggle <cr>" },
+		{ "<leader>ca", "<cmd> ColorizerAttachToBuffer <cr>" },
+		{ "<leader>cd", "<cmd> ColorizerDetachFromBuffer <cr>" },
+		{ "<leader>cr", "<cmd> ColorizerReloadAllBuffers <cr>" },
+	},
 }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,21 +1,17 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	event = "BufReadPost",
+	event = "VeryLazy",
 	cmd = "Gitsigns",
 	opts = {},
 	keys = {
-		{
-			"[c",
-			function()
-				require("gitsigns").nav_hunk("prev")
-			end,
-		},
-		{
-			"]c",
-			function()
-				require("gitsigns").nav_hunk("next")
-			end,
-		},
+		{ "[g", "<cmd> Gitsigns prev_hunk <cr>" },
+		{ "]g", "<cmd> Gitsigns next_hunk <cr>" },
+		{ "<leader>gv", "<cmd> Gitsigns select_hunk <cr>" },
+		{ "<leader>gw", "<cmd> Gitsigns toggle_word_diff <cr>" },
+		{ "<leader>grb", "<cmd> Gitsigns reset_buffer <cr>" },
+		{ "<leader>grh", "<cmd> Gitsigns reset_hunk <cr>", mode = { "n", "v" } },
+		{ "<leader>gd", "<cmd> Gitsigns diffthis <cr>", mode = { "n", "v" } },
+		{ "<leader>gp", "<cmd> Gitsigns preview_hunk_inline <cr>", mode = { "n", "v" } },
 		-- TODO: настроить клавиши
 	},
 }
